6cda81ca104a778d7ad04f9402cdc915a439a738
Godwin Nwachukwu <nwgwin@gmail.com>
2019-11-23 22:34:27 +0100

    got the app to a working state, but this is spagetti code

--- colorgame.js
+++ colorgame.js
@@ -4,14 +4,15 @@ let askedColor = document.querySelector(".asked-color");
 let headerBackground = document.querySelector("header");
 let gameStatus = document.querySelector(".status");
 let freshGameButton = document.querySelector(".freshgame");
+let easyButton = document.querySelector(".easy");
+let hardButton = document.querySelector(".hard");
 let pickedColor = pickColor();
+let containerDiv = document.querySelector(".container");
 askedColor.innerHTML = pickedColor;
 gameStatus.innerHTML = "";
+hardButton.classList.add("selected");
 
-function pickColor(){
-    let random = colors[Math.floor(Math.random()*colors.length)];
-    return random;
-}
+
 
 freshGameButton.addEventListener("click", function(){
     //gemnerate random colors
@@ -24,7 +25,117 @@ freshGameButton.addEventListener("click", function(){
     for (let i = 0; i < colorSquares.length; i++){
         colorSquares[i].style.backgroundColor = colors[i];
     }
-    headerBackground.style.backgroundColor = "steelblue"
+    headerBackground.style.backgroundColor = "steelblue";
+    freshGameButton.innerHTML = "NEW GAME"
+    
+})
+
+easyButton.addEventListener("click", function(){
+    easyButton.classList.add("selected");
+    hardButton.classList.remove("selected");
+    //change DOM Fields to 3 colors
+    containerDiv.innerHTML = "<div class='color-square'></div><div class='color-square'></div><div class='color-square'></div>";
+     //select the colors nodeList
+    let colorSquares = document.querySelectorAll(".color-square");
+    //generate random colors
+    colors = generateColors(3);
+   //pick one random colors
+    pickedColor = pickColor();
+    //reflect the pickedColor in the question area
+    askedColor.innerHTML = pickedColor;
+    //change color of squares
+    function rightColor(color) {
+        for (let i = 0; i < colors.length; i++) {
+            colorSquares[i].style.backgroundColor = color;
+        }
+    }
+    for (let i = 0; i < colorSquares.length; i++){
+        colorSquares[i].style.backgroundColor = colors[i];
+        colorSquares[i].addEventListener("click", function() {
+            let clickedColor = this.style.backgroundColor;
+            if (clickedColor !== pickedColor) {
+                gameStatus.innerHTML = "Try again!"
+                this.style.backgroundColor = "black";
+            } else {
+                gameStatus.innerHTML = "Correct!";
+                rightColor(pickedColor);
+                headerBackground.style.backgroundColor = pickedColor;
+                freshGameButton.innerHTML = "PLAY AGAIN!";
+                easyButton.style.backgroundColor = "pickedColor";
+            }      
+        })
+    }
+    headerBackground.style.backgroundColor = "steelblue";
+    freshGameButton.innerHTML = "NEW GAME"
+
+    freshGameButton.addEventListener("click", function(){
+        //gemnerate random colors
+        colors = generateColors(3);
+        //pick one random colors
+        pickedColor = pickColor();
+        //reflect the pickedColor in the question area
+        askedColor.innerHTML = pickedColor;
+        //change color of squares
+        for (let i = 0; i < colorSquares.length; i++){
+            colorSquares[i].style.backgroundColor = colors[i];
+        }
+        headerBackground.style.backgroundColor = "steelblue";
+        freshGameButton.innerHTML = "NEW GAME"
+
+    })
+})
+
+hardButton.addEventListener("click", function(){
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    containerDiv.innerHTML = "<div class='color-square'></div><div class='color-square'></div><div class='color-square'></div><div class='color-square'></div><div class='color-square'></div><div class='color-square'></div>";
+    let colorSquares = document.querySelectorAll(".color-square");
+     //gemnerate random colors
+     colors = generateColors(6);
+     //pick one random colors
+     pickedColor = pickColor();
+     //reflect the pickedColor in the question area
+     askedColor.innerHTML = pickedColor;
+     //change color of squares
+     for (let i = 0; i < colorSquares.length; i++){
+         colorSquares[i].style.backgroundColor = colors[i];
+         colorSquares[i].addEventListener("click", function() {
+            let clickedColor = this.style.backgroundColor;
+            if (clickedColor !== pickedColor) {
+                gameStatus.innerHTML = "Try again!"
+                this.style.backgroundColor = "black";
+            } else {
+                gameStatus.innerHTML = "Correct!";
+                rightColor(pickedColor);
+                headerBackground.style.backgroundColor = pickedColor;
+                freshGameButton.innerHTML = "PLAY AGAIN!";
+                easyButton.style.backgroundColor = "pickedColor";
+            }      
+        })
+     }
+     headerBackground.style.backgroundColor = "steelblue";
+     freshGameButton.innerHTML = "NEW GAME";
+     function rightColor(color) {
+        for (let i = 0; i < colors.length; i++) {
+            colorSquares[i].style.backgroundColor = color;
+        }
+    }
+    freshGameButton.addEventListener("click", function(){
+        //gemnerate random colors
+        colors = generateColors(6);
+        //pick one random colors
+        pickedColor = pickColor();
+        //reflect the pickedColor in the question area
+        askedColor.innerHTML = pickedColor;
+        //change color of squares
+        for (let i = 0; i < colorSquares.length; i++){
+            colorSquares[i].style.backgroundColor = colors[i];
+        }
+        headerBackground.style.backgroundColor = "steelblue";
+        freshGameButton.innerHTML = "NEW GAME"
+        
+    })
+     
 })
 
 for (let i = 0; i < colorSquares.length; i++){
@@ -38,6 +149,7 @@ for (let i = 0; i < colorSquares.length; i++){
             gameStatus.innerHTML = "Correct!";
             rightColor(pickedColor);
             headerBackground.style.backgroundColor = pickedColor;
+            freshGameButton.innerHTML = "PLAY AGAIN!"
         }      
     })
 }
@@ -61,6 +173,11 @@ function getRandomColors(){
     let g = Math.floor(Math.random()*256);
     let b = Math.floor(Math.random()*256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function pickColor(){
+    let random = colors[Math.floor(Math.random()*colors.length)];
+    return random;
 }
 
 
